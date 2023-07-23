12fb8930611ff274c20fd3f7b1f7e4b9ecaa39d2
KabongeHajarah <kabongehajarah@gmail.com>
2023-07-23 22:23:21 +0400

    Making cart quantity on the page interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -77,9 +77,17 @@ if(productId===item.productName){
         productId:productId,
         quantity:1
       });
-  }  
-      
-      
+  } 
+  let cartQuantity=0; 
+     cart.forEach((item)=>{
+cartQuantity+=item.quantity
+     })
+     
+     document.querySelector('.js-cart-quantity').
+     innerHTML=cartQuantity;
+
+
+      console.log(cartQuantity);
       console.log(cart);
     });
 });
